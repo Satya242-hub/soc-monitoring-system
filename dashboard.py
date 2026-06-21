8069c62577efdc011d22b3910e273a7688f28c96
Satya242-hub <shivasatyareddy02@gmail.com>
2026-06-21 11:47:43 +0530

    Update API URL and maintain dashboard functionality

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = https://soc-monitoring-system.onrender.com
 
 st.set_page_config(page_title="SOC Dashboard", layout="wide")
 st.title("🛡️ SOC Monitoring Dashboard")
